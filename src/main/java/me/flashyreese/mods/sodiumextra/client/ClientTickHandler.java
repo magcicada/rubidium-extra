2a797c1782f9c6eea5580e5e54b2bc4aee1aa41d
dima_dencep <dima_dencep@turbomc.tk>
2022-08-03 09:03:55 +0700

    port to forge

--- src/main/java/me/flashyreese/mods/sodiumextra/client/ClientTickHandler.java
+++ src/main/java/me/flashyreese/mods/sodiumextra/client/ClientTickHandler.java
@@ -2,7 +2,8 @@ package me.flashyreese.mods.sodiumextra.client;
 
 import me.flashyreese.mods.sodiumextra.common.util.EvictingQueue;
 import me.flashyreese.mods.sodiumextra.mixin.gui.MinecraftClientAccessor;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.TickEvent;
 
 import java.util.Queue;
 
@@ -10,10 +11,12 @@ public class ClientTickHandler {
     private final Queue<Integer> averageFps = new EvictingQueue<>(200);
 
     public void onClientInitialize() {
-        ClientTickEvents.START_CLIENT_TICK.register(minecraftClient -> {
-            int currentFPS = MinecraftClientAccessor.getCurrentFPS();
-            this.averageFps.add(currentFPS);
-        });
+        MinecraftForge.EVENT_BUS.addListener(this::onTick);
+    }
+
+    public void onTick(TickEvent.ClientTickEvent event) {
+        int currentFPS = MinecraftClientAccessor.getCurrentFPS();
+        this.averageFps.add(currentFPS);
     }
 
     public int getAverageFps() {
--- src/main/java/me/flashyreese/mods/sodiumextra/mixin/sodium/resolution/MixinSodiumGameOptionPages.java
+++ src/main/java/me/flashyreese/mods/sodiumextra/mixin/sodium/resolution/MixinSodiumGameOptionPages.java
@@ -20,7 +20,7 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import java.util.List;
 import java.util.Optional;
 
-@Mixin(SodiumGameOptionPages.class)
+@Mixin(value = SodiumGameOptionPages.class, remap = false)
 public class MixinSodiumGameOptionPages {
 
     @Shadow
@@ -34,7 +34,7 @@ public class MixinSodiumGameOptionPages {
         groups.add(OptionGroup.createBuilder()
                 .add(OptionImpl.createBuilder(int.class, vanillaOpts)
                         .setName(new TranslatableText("options.fullscreen.resolution").getString())
-                        .setTooltip(new TranslatableText("sodium-extra.option.resolution.tooltip").getString())
+                        .setTooltip(new TranslatableText("sodiumextra.option.resolution.tooltip").getString())
                         .setControl(option -> new SliderControlExtended(option, 0, MinecraftClient.getInstance().getWindow().getMonitor() != null ? MinecraftClient.getInstance().getWindow().getMonitor().getVideoModeCount() : 0, 1, ControlValueFormatterExtended.resolution(), true))
                         .setBinding((options, value) -> {
                             if (window.getMonitor() != null) {
