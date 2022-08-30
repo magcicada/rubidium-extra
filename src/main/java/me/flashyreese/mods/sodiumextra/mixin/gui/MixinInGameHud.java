d30b35e4f15f1cabd65e1864d58d293e7ed33d48
dima_dencep <dima_dencep@turbomc.tk>
2022-08-30 21:15:12 +0700

    rename

--- src/main/java/me/flashyreese/mods/sodiumextra/mixin/gui/MixinInGameHud.java
+++ src/main/java/me/flashyreese/mods/sodiumextra/mixin/gui/MixinInGameHud.java
@@ -17,9 +17,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ForgeIngameGui.class)
-public abstract class MixinForgeIngameGui extends InGameHud {
+public abstract class MixinInGameHud extends InGameHud {
 
-    public MixinForgeIngameGui(MinecraftClient client) {
+    public MixinInGameHud(MinecraftClient client) {
         super(client);
     }
 
