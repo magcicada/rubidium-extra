6775de0030220c36e1c7a7979fef558e6c79e733
dima_dencep <dima_dencep@constructlegacy.ru>
2023-04-30 19:56:09 +0700

    Fix removing star

--- src/main/java/me/flashyreese/mods/sodiumextra/client/compat/CitadelStarFix.java
+++ src/main/java/me/flashyreese/mods/sodiumextra/client/compat/CitadelStarFix.java
@@ -1,2 +1,16 @@
-package me.flashyreese.mods.sodiumextra.client.compat;public class CitadelStarFix {
+package me.flashyreese.mods.sodiumextra.client.compat;
+
+import com.github.alexthe666.citadel.client.event.EventGetStarBrightness;
+import me.flashyreese.mods.sodiumextra.client.SodiumExtraClientMod;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class CitadelStarFix {
+    @SubscribeEvent
+    public void onGetStarBrightness(EventGetStarBrightness event) {
+        if (!SodiumExtraClientMod.options().detailSettings.stars) {
+            event.setBrightness(0.0f);
+            event.setResult(Event.Result.ALLOW);
+        }
+    }
 }
