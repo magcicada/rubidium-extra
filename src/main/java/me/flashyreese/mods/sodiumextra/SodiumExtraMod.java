fe80493b887550634b894890fd5860a6a5e4dd3e
Paint_Ninja <PaintNinja@users.noreply.github.com>
2023-05-18 21:07:07 +0100

    Fix displaytest
    Whoops, seems like Forge's displayTest mods.toml property is knackered atm, The JiJ'ed colamixinconfig is still causing the displaytest to fail however.

--- src/main/java/me/flashyreese/mods/sodiumextra/SodiumExtraMod.java
+++ src/main/java/me/flashyreese/mods/sodiumextra/SodiumExtraMod.java
@@ -1,9 +1,14 @@
 package me.flashyreese.mods.sodiumextra;
 
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.network.NetworkConstants;
 
 @Mod(SodiumExtraMod.MOD_ID)
 public final class SodiumExtraMod {
     public static final String MOD_ID = "sodiumextra";
-    public SodiumExtraMod() {}
+    public SodiumExtraMod() {
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+    }
 }
