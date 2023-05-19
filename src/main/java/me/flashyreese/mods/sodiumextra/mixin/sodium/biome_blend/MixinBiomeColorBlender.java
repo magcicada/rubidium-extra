c798ddd74bc358c96fa770a874b7cf04927db9e8
dima_dencep <dima_dencep@constructlegacy.ru>
2023-05-20 05:40:57 +0700

    fix upstream

--- src/main/java/me/flashyreese/mods/sodiumextra/mixin/sodium/biome_blend/MixinBiomeColorBlender.java
+++ src/main/java/me/flashyreese/mods/sodiumextra/mixin/sodium/biome_blend/MixinBiomeColorBlender.java
@@ -2,9 +2,9 @@ package me.flashyreese.mods.sodiumextra.mixin.sodium.biome_blend;
 
 import me.flashyreese.mods.sodiumextra.client.SodiumExtraClientMod;
 import me.jellysquid.mods.sodium.client.model.quad.ModelQuadView;
-import me.jellysquid.mods.sodium.client.model.quad.blender.BiomeColorBlender;
+import me.jellysquid.mods.sodium.client.model.quad.blender.LinearColorBlender;
 import me.jellysquid.mods.sodium.client.model.quad.blender.ColorSampler;
-import net.caffeinemc.mods.sodium.api.util.ColorARGB;
+import me.jellysquid.mods.sodium.client.util.color.ColorARGB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockRenderView;
 import org.spongepowered.asm.mixin.Final;
@@ -16,24 +16,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Arrays;
 
-@Mixin(value = BiomeColorBlender.class, remap = false)
+@Mixin(value = LinearColorBlender.class, remap = false)
 public abstract class MixinBiomeColorBlender {
-    @Shadow
-    @Final
-    private boolean useSmoothBlending;
-    @Shadow
-    @Final
-    private int[] cachedRet;
+    @Shadow @Final private int[] cachedRet;
 
-    @Shadow
-    protected abstract <T> void getColorsLinear(BlockRenderView world, BlockPos origin, ModelQuadView quad, ColorSampler<T> sampler, T state, int[] colors);
+    @Shadow protected abstract <T> int getVertexColor(BlockRenderView world, BlockPos origin, ModelQuadView quad, ColorSampler<T> sampler, T state, int vertexIdx);
 
     /**
      * @author FlashyReese
      */
     @Inject(method = "getColors", at = @At(value = "HEAD"), cancellable = true)
     private <T> void getColors(BlockRenderView world, BlockPos origin, ModelQuadView quad, ColorSampler<T> sampler, T state, CallbackInfoReturnable<int[]> cir) {
-        if (this.useSmoothBlending && SodiumExtraClientMod.options().renderSettings.useLinearFlatColorBlender) {
+        if (SodiumExtraClientMod.options().renderSettings.useLinearFlatColorBlender) {
             int[] colors = this.cachedRet;
             this.getColorsLinearFlat(world, origin, quad, sampler, state, colors);
             cir.setReturnValue(colors);
@@ -41,7 +35,9 @@ public abstract class MixinBiomeColorBlender {
     }
 
     private <T> void getColorsLinearFlat(BlockRenderView world, BlockPos origin, ModelQuadView quad, ColorSampler<T> sampler, T state, int[] colors) {
-        this.getColorsLinear(world, origin, quad, sampler, state, colors);
+        for(int vertexIndex = 0; vertexIndex < 4; ++vertexIndex) {
+            colors[vertexIndex] = this.getVertexColor(world, origin, quad, sampler, state, vertexIndex);
+        }
 
         int a = Arrays.stream(colors).map(ColorARGB::unpackAlpha).sum();
         int r = Arrays.stream(colors).map(ColorARGB::unpackRed).sum();
