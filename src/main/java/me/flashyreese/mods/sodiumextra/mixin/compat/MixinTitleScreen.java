c3ab5fc20101d568c7ec17355e565c08041a85a2
dima_dencep <dima_dencep@constructlegacy.ru>
2023-08-09 02:18:30 +0700

    Suggest TRO

--- src/main/java/me/flashyreese/mods/sodiumextra/mixin/compat/MixinTitleScreen.java
+++ src/main/java/me/flashyreese/mods/sodiumextra/mixin/compat/MixinTitleScreen.java
@@ -18,11 +18,11 @@ public abstract class MixinTitleScreen extends Screen {
         super(title);
     }
 
-    /*@Inject(method = "init", at = @At(value = "RETURN")) TODO
+    @Inject(method = "init", at = @At(value = "RETURN"))
     private void postInit(CallbackInfo ci) {
         if (!ModList.get().isLoaded("reeses_sodium_options") && !SodiumExtraClientMod.options().notificationSettings.hideRSORecommendation && !SodiumExtraClientMod.options().hasSuggestedRSO()) {
             this.client.setScreen(new SuggestRSOScreen(this));
             SodiumExtraClientMod.options().setSuggestedRSO(true);
         }
-    }*/
+    }
 }
