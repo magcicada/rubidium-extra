fd8c29f85e1706cf1ca0f18de6eb8e9134fde638
dima_dencep <dima_dencep@constructlegacy.ru>
2023-09-06 20:12:38 +0700

    Update for last sodium

--- src/main/java/me/flashyreese/mods/sodiumextra/mixin/compat/MixinTitleScreen.java
+++ src/main/java/me/flashyreese/mods/sodiumextra/mixin/compat/MixinTitleScreen.java
@@ -18,11 +18,11 @@ public abstract class MixinTitleScreen extends Screen {
         super(title);
     }
 
-    @Inject(method = "init", at = @At(value = "RETURN"))
+    /*@Inject(method = "init", at = @At(value = "RETURN")) TODO
     private void postInit(CallbackInfo ci) {
         if (!ModList.get().isLoaded("reeses_sodium_options") && !SodiumExtraClientMod.options().notificationSettings.hideRSORecommendation && !SodiumExtraClientMod.options().hasSuggestedRSO()) {
             this.client.setScreen(new SuggestRSOScreen(this));
             SodiumExtraClientMod.options().setSuggestedRSO(true);
         }
-    }
+    }*/
 }
