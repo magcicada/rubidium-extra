833862053d701e9bc37bd726d645d5a6cbff1349
dima_dencep <dima_dencep@constructlegacy.ru>
2023-09-25 17:02:37 +0700

    Backport fixes from 1.20.1

--- src/main/java/me/flashyreese/mods/sodiumextra/SodiumExtraMod.java
+++ src/main/java/me/flashyreese/mods/sodiumextra/SodiumExtraMod.java
@@ -7,7 +7,7 @@ import net.minecraftforge.network.NetworkConstants;
 
 @Mod(SodiumExtraMod.MOD_ID)
 public final class SodiumExtraMod {
-    public static final String MOD_ID = "sodiumextra";
+    public static final String MOD_ID = "rubidium_extra";
     public SodiumExtraMod() {
         ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
     }
