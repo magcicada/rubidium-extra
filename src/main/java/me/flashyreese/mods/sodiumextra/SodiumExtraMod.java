c2a1f39b1d5db159e80503acf2dd9464340b42e6
dima_dencep <dima_dencep@constructlegacy.ru>
2023-10-29 17:53:00 +0700

    Oops

--- src/main/java/me/flashyreese/mods/sodiumextra/SodiumExtraMod.java
+++ src/main/java/me/flashyreese/mods/sodiumextra/SodiumExtraMod.java
@@ -1,14 +1,15 @@
 package me.flashyreese.mods.sodiumextra;
 
-import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.network.NetworkConstants;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 
 @Mod(SodiumExtraMod.MOD_ID)
 public final class SodiumExtraMod {
     public static final String MOD_ID = "sodiumextra";
     public SodiumExtraMod() {
-        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
     }
 }
