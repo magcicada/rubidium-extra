62b70204f73a33a36a9f4e3f93ba0c604a6649e3
dima_dencep <dima_dencep@constructlegacy.ru>
2023-11-17 04:38:19 +0700

    Remove mixin export

--- src/main/java/me/flashyreese/mods/sodiumextra/mixin/fog/MixinBackgroundRenderer.java
+++ src/main/java/me/flashyreese/mods/sodiumextra/mixin/fog/MixinBackgroundRenderer.java
@@ -11,7 +11,6 @@ import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.CameraSubmersionType;
 import net.minecraft.entity.Entity;
 import org.jetbrains.annotations.Nullable;
-import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -19,7 +18,6 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Debug(export = true)
 @Mixin(BackgroundRenderer.class)
 public abstract class MixinBackgroundRenderer {
     @Shadow
