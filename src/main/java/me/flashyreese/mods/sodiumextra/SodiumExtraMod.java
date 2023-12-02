04a25d6c2a58a0bcb893c2f51f8351463b820fe1
dima_dencep <dima_dencep@constructlegacy.ru>
2023-12-02 20:59:45 +0700

    Fix #65

--- src/main/java/me/flashyreese/mods/sodiumextra/SodiumExtraMod.java
+++ src/main/java/me/flashyreese/mods/sodiumextra/SodiumExtraMod.java
@@ -1,14 +1,10 @@
 package me.flashyreese.mods.sodiumextra;
 
-import net.neoforged.fml.IExtensionPoint;
-import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.neoforge.network.NetworkConstants;
 
 @Mod(SodiumExtraMod.MOD_ID)
 public final class SodiumExtraMod {
     public static final String MOD_ID = "rubidium_extra";
     public SodiumExtraMod() {
-        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
     }
 }
