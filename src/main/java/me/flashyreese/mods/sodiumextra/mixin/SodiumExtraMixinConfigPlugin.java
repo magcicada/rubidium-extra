6289b939a4acb99fe7eada170404ff7ed224b8ef
dima_dencep <dima_dencep@redlance.org>
2024-07-02 02:33:28 +0700

    Hacks for embeddium

--- src/main/java/me/flashyreese/mods/sodiumextra/mixin/SodiumExtraMixinConfigPlugin.java
+++ src/main/java/me/flashyreese/mods/sodiumextra/mixin/SodiumExtraMixinConfigPlugin.java
@@ -1,12 +1,28 @@
 package me.flashyreese.mods.sodiumextra.mixin;
 
+import com.llamalad7.mixinextras.utils.MixinInternals;
 import me.flashyreese.mods.sodiumextra.client.SodiumExtraClientMod;
 import net.caffeinemc.caffeineconfig.AbstractCaffeineConfigMixinPlugin;
 import net.caffeinemc.caffeineconfig.CaffeineConfig;
+import org.embeddedt.embeddium.impl.taint.mixin.MixinTaintDetector;
+import org.spongepowered.asm.mixin.transformer.ext.IExtension;
+
+import java.lang.reflect.Field;
 
 public class SodiumExtraMixinConfigPlugin extends AbstractCaffeineConfigMixinPlugin {
 
     private static final String MIXIN_PACKAGE_ROOT = "me.flashyreese.mods.sodiumextra.mixin.";
+
+    public SodiumExtraMixinConfigPlugin() {
+        System.setProperty("embeddium.mixinTaintEnforceLevel", "WARN");
+        try {
+            Field instanceField = MixinTaintDetector.class.getDeclaredField("INSTANCE");
+            instanceField.setAccessible(true);
+            MixinInternals.unregisterExtension((IExtension) instanceField.get(null));
+        } catch (Throwable th) {
+            System.out.println("Embeddium is shit");
+        }
+    }
 
     @Override
     protected CaffeineConfig createConfig() {
