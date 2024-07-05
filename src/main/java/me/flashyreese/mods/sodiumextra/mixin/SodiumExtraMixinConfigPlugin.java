d0f711b7d69500e5c7cae2ab7a57e33519861d87
dima_dencep <dima_dencep@redlance.org>
2024-07-05 14:40:10 +0700

    Update config to hack embeddium

--- src/main/java/me/flashyreese/mods/sodiumextra/mixin/SodiumExtraMixinConfigPlugin.java
+++ src/main/java/me/flashyreese/mods/sodiumextra/mixin/SodiumExtraMixinConfigPlugin.java
@@ -1,10 +1,8 @@
 package me.flashyreese.mods.sodiumextra.mixin;
 
-import com.llamalad7.mixinextras.utils.MixinInternals;
 import me.flashyreese.mods.sodiumextra.client.SodiumExtraClientMod;
 import net.caffeinemc.caffeineconfig.AbstractCaffeineConfigMixinPlugin;
 import net.caffeinemc.caffeineconfig.CaffeineConfig;
-import org.spongepowered.asm.mixin.transformer.ext.IExtension;
 
 public class SodiumExtraMixinConfigPlugin extends AbstractCaffeineConfigMixinPlugin {
 
